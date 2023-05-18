05a1f20faff2a5af3e5c1fdbcda87c3fb949a97e
CharoitAS <120410812+CharoitAS@users.noreply.github.com>
2023-05-18 21:52:16 +0300

    Возраст трёх человек

--- minimax/src/main/java/mari/App.java
+++ minimax/src/main/java/mari/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /**System.out.println( "Hello World!" );
         int vasyaAge = 12;
         int nastyaAge = 40;
 
@@ -28,6 +28,37 @@ public class App
         }
         System.out.println("Минимальный возраст " + min);
         System.out.println("Максимальный возраст " + max);
+*/
+        
+        int vasyaAge = 12;
+        int nastyaAge = 40;
+        int kolyaAge = 36;
 
+        int min = -1; 
+        int max = -1;
+        int middle = -1; 
+    
+
+        if (vasyaAge < nastyaAge && vasyaAge < kolyaAge) {
+            min = vasyaAge;
+
+        } else if (nastyaAge < kolyaAge && nastyaAge < vasyaAge) {
+            min = nastyaAge;
+
+        } else {
+            min = kolyaAge;
+        }
+        
+        if (vasyaAge > nastyaAge && vasyaAge > kolyaAge) {
+            max = vasyaAge;
+
+        } else if (nastyaAge > kolyaAge && nastyaAge > vasyaAge) {
+            max = nastyaAge;
+
+        } else {
+            max = kolyaAge;
+        }
+        System.out.println("Минимальный возраст " + min);
+        System.out.println("Максимальный возраст " + max);
     }
 }
