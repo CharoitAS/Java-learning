2647a71881b7b1ad653da1a6dc3477fee97bf11a
CharoitAS <120410812+CharoitAS@users.noreply.github.com>
2023-05-18 21:04:44 +0300

    Тернарный оператор

--- coffee/src/main/java/mari/App.java
+++ coffee/src/main/java/mari/App.java
@@ -24,19 +24,16 @@ public class App
         int milkAmount = 4000;
 
         int coffeePerPortion = 15;
-        int milkPerPortion = 100;
+        int milkPerPortion = choiceCoffee == 2 ? 0 : 100; // если выбран кофе без молока то 0 иначе 100 (0 - кофе без молока, 100 - кофе с молоком)
 
-        if(choiceCoffee == 2) {
-            milkPerPortion = 0;
-        }
-        int totalCoffeNeed = coffeePortions*coffeePerPortion;
-        int totalMilkNeed = coffeePortions*milkPerPortion;
-        
-        if(coffeeAmount >= totalCoffeNeed && milkAmount >= totalMilkNeed) {
+        int totalCoffeNeed = coffeePortions * coffeePerPortion;
+        int totalMilkNeed = coffeePortions * milkPerPortion;
+
+        if (coffeeAmount >= totalCoffeNeed && milkAmount >= totalMilkNeed) {
             System.out.println("Кофе готовится");
         } else {
             System.out.println("Кофе не готовится, недостаточно ингредиентов");
-            
+
         }
         in.close();
 
