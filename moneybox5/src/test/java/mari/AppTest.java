2fdafb2e3e5a368b95f9d4032568433588d64adf
CharoitAS <120410812+CharoitAS@users.noreply.github.com>
2023-06-20 22:46:12 +0300

    Тесты setAmount

--- moneybox5/src/test/java/mari/AppTest.java
+++ moneybox5/src/test/java/mari/AppTest.java
@@ -18,6 +18,7 @@ public class AppTest
         Assert.assertEquals(stack.getAmount(), 0); //изначально монет в стопке нет
         Assert.assertEquals(stack.getRub(), 0); // начальная стоимость стопки 0 рублей
         Assert.assertEquals(stack.getCoinValue(), 1); // в стопке однорублёвые монеты
+        Assert.assertEquals(stack.toString(), "С номиналом 1 руб. - 0 шт.");
     }
     @Test
     public void checkPutCoin() 
@@ -27,6 +28,7 @@ public class AppTest
         Assert.assertEquals(stack.getAmount(), 1); // в стопке одна монета
         Assert.assertEquals(stack.getRub(), 1); // в стопке один рубль
         Assert.assertEquals(stack.getCoinValue(), 1); // в стопке однорублёвые монеты
+        Assert.assertEquals(stack.toString(), "С номиналом 1 руб. - 1 шт.");
     }
     @Test
     public void checkPutCoin2() 
@@ -38,6 +40,7 @@ public class AppTest
         Assert.assertEquals(stack.getAmount(), 2); // в стопке две монеты
         Assert.assertEquals(stack.getRub(), 10); // в стопке десять рублей
         Assert.assertEquals(stack.getCoinValue(), 5); // в стопке пятирублёвые монеты
+        Assert.assertEquals(stack.toString(), "С номиналом 5 руб. - 2 шт.");
     }
     @Test
     public void checkPutCoin3() 
@@ -50,17 +53,28 @@ public class AppTest
         
         Assert.assertEquals(stack.getAmount(), 100); // в стопке сто монет
         Assert.assertEquals(stack.getRub(), 1000); // в стопке тысяча рублей
+        Assert.assertEquals(stack.toString(), "С номиналом 10 руб. - 100 шт.");
         
-    }    
-
+    } 
     @Test
-    public void checkPutCoin4()
+    public void checkSetAmount1()
     {
-        CoinsStack stack = new CoinsStack(1);
-        Assert.assertEquals(stack.getAmount(), 0); //изначально монет в стопке нет
-        Assert.assertEquals(stack.getRub(), 0); // начальная стоимость стопки 0 рублей
-        Assert.assertEquals(stack.getCoinValue(), 1); // в стопке однорублёвые монеты
-        Assert.assertEquals(stack.toString(), "С номиналом 1 руб. - 0 шт.");
-    }  
-        
+        CoinsStack stack = new CoinsStack(50);
+        stack.setAmount(12);
+        Assert.assertEquals(stack.getAmount(), 12);
+        Assert.assertEquals(stack.getRub(), 600);
+        Assert.assertEquals(stack.getCoinValue(), 50);
+        Assert.assertEquals(stack.toString(), "С номиналом 50 руб. - 12 шт.");        
+    }
+     @Test
+    public void checkSetAmount()
+    {
+        CoinsStack stack = new CoinsStack(3);
+        stack.setAmount(12);
+        stack.setAmount(15);
+        Assert.assertEquals(stack.getAmount(), 15);
+        Assert.assertEquals(stack.getRub(), 45);
+        Assert.assertEquals(stack.getCoinValue(), 3);
+        Assert.assertEquals(stack.toString(), "С номиналом 3 руб. - 15 шт.");
+    }
 }
