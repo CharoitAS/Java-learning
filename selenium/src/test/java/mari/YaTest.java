b969a398fb8ac9c2662288a1598c59ec4560d53d
CharoitAS <120410812+CharoitAS@users.noreply.github.com>
2023-06-29 21:52:54 +0300

    Добавили вызов sleep

--- selenium/src/test/java/mari/YaTest.java
+++ selenium/src/test/java/mari/YaTest.java
@@ -1,5 +1,7 @@
 package mari;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.lang.Thread;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class YaTest {
@@ -22,6 +26,7 @@ public class YaTest {
     @BeforeEach
     void setup() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterEach
@@ -30,12 +35,14 @@ public class YaTest {
     }
     
     @Test
-    public void findVK()
+    public void findVK() throws InterruptedException
     {
         driver.navigate().to("https://ya.ru/");
         driver.findElement(By.id("text")).sendKeys("Вк");
         driver.findElement(By.className("search3__button")).click();
+        Thread.sleep(1000); // подождать 1 секунду
         String title = driver.getTitle();
+        System.out.println(title);
         Assertions.assertTrue(title.startsWith("Вк — Яндекс: ")); //("Вк — Яндекс: нашлось 27 млн результатов", title);
         String text = driver.findElement(By.className("organic__greenurl")).getText();
         Assertions.assertEquals("vk.com", text);
